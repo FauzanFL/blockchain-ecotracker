fa4ddb287c637795e2643f2de4a40edfb05402e5
fauzan.lisan <fauzan.lisan@solusi247.com>
2026-02-06 13:20:04 +0700

    add env example and fix cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
